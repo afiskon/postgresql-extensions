f284051ac16a25c94977a2b030b685aa94de1667
Aleksander Alekseev <mail@eax.me>
2022-02-17 13:00:58 +0300

    Formatting

--- 002-datum-and-procedure-call/experiment.c
+++ 002-datum-and-procedure-call/experiment.c
@@ -10,42 +10,42 @@ PG_FUNCTION_INFO_V1(timestamptz_zone_test);
 Datum
 timestamp_in_out_test(PG_FUNCTION_ARGS)
 {
-	const char* tstamp_str = "Jan 01 00:00:00 2010";
-	Timestamp tstamp;
+    const char* tstamp_str = "Jan 01 00:00:00 2010";
+    Timestamp tstamp;
 
-	tstamp = DatumGetTimestamp(DirectFunctionCall3(timestamp_in,
-		CStringGetDatum(tstamp_str),
-		ObjectIdGetDatum(InvalidOid),
-		Int32GetDatum(-1)));
+    tstamp = DatumGetTimestamp(DirectFunctionCall3(timestamp_in,
+        CStringGetDatum(tstamp_str),
+        ObjectIdGetDatum(InvalidOid),
+        Int32GetDatum(-1)));
 
-	tstamp_str = DatumGetCString(DirectFunctionCall1(timestamp_out,
-		TimestampGetDatum(tstamp)));
+    tstamp_str = DatumGetCString(DirectFunctionCall1(timestamp_out,
+        TimestampGetDatum(tstamp)));
 
-	PG_RETURN_TEXT_P(CStringGetTextDatum(tstamp_str));
+    PG_RETURN_TEXT_P(CStringGetTextDatum(tstamp_str));
 }
 
 Datum
 timestamptz_zone_test(PG_FUNCTION_ARGS)
 {
-	Timestamp result;
-	TimestampTz timestamptz = TimestampTzGetDatum(PG_GETARG_DATUM(0));
-	const char* tzname = "Europe/Moscow";
+    Timestamp result;
+    TimestampTz timestamptz = TimestampTzGetDatum(PG_GETARG_DATUM(0));
+    const char* tzname = "Europe/Moscow";
 
-	if(TIMESTAMP_NOT_FINITE(timestamptz)) {
-		// Timestamp and TimestampTz internally are the same in PostgreSQL
-		// The TZ-version of TIMESTAMP_NOT_FINITE() is not even defined!
-		result = (Timestamp)timestamptz;
-		PG_RETURN_TIMESTAMP(result);
-	}
+    if(TIMESTAMP_NOT_FINITE(timestamptz)) {
+        // Timestamp and TimestampTz internally are the same in PostgreSQL
+        // The TZ-version of TIMESTAMP_NOT_FINITE() is not even defined!
+        result = (Timestamp)timestamptz;
+        PG_RETURN_TIMESTAMP(result);
+    }
 
-	if(PG_NARGS() > 1) {
-		tzname = TextDatumGetCString(PG_GETARG_DATUM(1));
-	}
+    if(PG_NARGS() > 1) {
+        tzname = TextDatumGetCString(PG_GETARG_DATUM(1));
+    }
 
-	// The code is equal to 'timestamptz AT TIME ZONE tzname'
-	result = DatumGetTimestamp(DirectFunctionCall2(timestamptz_zone,
-			CStringGetTextDatum(tzname),
-			TimestampTzGetDatum(timestamptz)));
+    // The code is equal to 'timestamptz AT TIME ZONE tzname'
+    result = DatumGetTimestamp(DirectFunctionCall2(timestamptz_zone,
+            CStringGetTextDatum(tzname),
+            TimestampTzGetDatum(timestamptz)));
 
-	PG_RETURN_TIMESTAMP(result);
+    PG_RETURN_TIMESTAMP(result);
 }
