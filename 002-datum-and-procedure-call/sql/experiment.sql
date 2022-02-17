18ff033949aa17c711fec9432bb5f4c8df703835
Aleksander Alekseev <mail@eax.me>
2022-02-17 12:46:46 +0300

    Add timestamptz_zone() call example

--- 002-datum-and-procedure-call/sql/experiment.sql
+++ 002-datum-and-procedure-call/sql/experiment.sql
@@ -1,5 +1,13 @@
 CREATE EXTENSION experiment;
 
 SELECT timestamp_in_out_test();
+SELECT timestamptz_zone_test(null);
+SELECT timestamptz_zone_test('-infinity');
+SELECT timestamptz_zone_test('2010-01-01 00:00:00 MSK');
+SELECT timestamptz_zone_test('2010-01-01 00:00:00 HST', 'Pacific/Honolulu');
+
+\set ON_ERROR_STOP 0
+SELECT timestamptz_zone_test('2010-01-01 00:00:00 GMT', 'Europe/Ololondon');
+\set ON_ERROR_STOP 1
 
 DROP EXTENSION experiment;
