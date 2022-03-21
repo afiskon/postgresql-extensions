fa618269fa58ea8e785e8d893f579c7ccd4b5a05
Aleksander Alekseev <mail@eax.me>
2022-03-21 16:07:25 +0300

    Add PG_TRY() example + code formatting fix

--- 003-logging-and-exceptions/experiment.c
+++ 003-logging-and-exceptions/experiment.c
@@ -5,26 +5,43 @@
 PG_MODULE_MAGIC;
 PG_FUNCTION_INFO_V1(experiment_elog);
 PG_FUNCTION_INFO_V1(experiment_ereport);
+PG_FUNCTION_INFO_V1(experiment_trycatch);
 
 Datum
 experiment_elog(PG_FUNCTION_ARGS)
 {
-	/* int64 now = (int64)GetCurrentTransactionStartTimestamp(); */
-	int64 now = 123456789;
+    /* int64 now = (int64)GetCurrentTransactionStartTimestamp(); */
+    int64 now = 123456789;
 
-	/* NB: %m corresponds to strerror(errno) */
-	elog(NOTICE, "Transaction start timestamp: %lld",
-		(long long)now);
-	PG_RETURN_VOID();
+    /* NB: %m corresponds to strerror(errno) */
+    elog(NOTICE, "Transaction start timestamp: %lld",
+        (long long)now);
+    PG_RETURN_VOID();
 }
 
 Datum
 experiment_ereport(PG_FUNCTION_ARGS)
 {
-	const char* arg = TextDatumGetCString(PG_GETARG_DATUM(0));
-	ereport(ERROR,
-		(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-		 errmsg("Invalid argument \"%s\"", arg),
-		 errhint("This is a hint message.")));
-	PG_RETURN_VOID();
+    const char* arg = TextDatumGetCString(PG_GETARG_DATUM(0));
+    ereport(ERROR,
+        (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+         errmsg("Invalid argument \"%s\"", arg),
+         errhint("This is a hint message.")));
+    PG_RETURN_VOID();
+}
+
+Datum
+experiment_trycatch(PG_FUNCTION_ARGS)
+{
+    PG_TRY();
+    {
+        elog(ERROR, "oops...");
+    }
+    PG_FINALLY();
+    {
+        elog(NOTICE, "cleaning up");
+    }
+    PG_END_TRY();
+
+    PG_RETURN_VOID();
 }
--- 003-logging-and-exceptions/sql/experiment.sql
+++ 003-logging-and-exceptions/sql/experiment.sql
@@ -3,7 +3,10 @@ CREATE EXTENSION experiment;
 SELECT experiment_elog();
 
 \set ON_ERROR_STOP 0
+
 SELECT experiment_ereport('hello');
+SELECT experiment_trycatch();
+
 \set ON_ERROR_STOP 1
 
 DROP EXTENSION experiment;
