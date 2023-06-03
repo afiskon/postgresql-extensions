cf8dfd79efe6c372931f3bcc85e99ed86b552939
Aleksander Alekseev <afiskon@gmail.com>
2023-06-03 16:11:58 +0300

    Minor changes

--- 007-gucs/t/001_basic.pl
+++ 007-gucs/t/001_basic.pl
@@ -16,18 +16,15 @@ $node->append_conf('postgresql.conf', qq{experiment.message = 'ololotrololo'});
 $node->start;
 
 $node->safe_psql("postgres", "CREATE EXTENSION experiment;");
-
 $result = $node->safe_psql("postgres", qq{
 SELECT experiment_get_message();
 });
-
 ok($result eq "ololotrololo", 'changing postgresql.conf has an effect');
 
 $result = $node->safe_psql("postgres", qq{
 SET experiment.message TO 'abc';
 SELECT experiment_get_message();
 });
-
 ok($result eq "abc", 'SET has an effect');
 
 $result = $node->safe_psql("postgres", qq{
@@ -35,11 +32,9 @@ SET experiment.message TO 'abc';
 RESET experiment.message;
 SELECT experiment_get_message();
 });
-
 ok($result eq "ololotrololo", 'RESET has an effect');
 
 $node->safe_psql("postgres", "DROP EXTENSION experiment;");
-
 $node->stop;
 
 done_testing();
